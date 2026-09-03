22340138c9977565eb57079d5480bae7bae2eaae
Nayan Patel <nayanpatel.dev@gmail.com>
2026-09-03 13:10:41 +0530

    Variables Update

--- Client/src/api/apiservice.js
+++ Client/src/api/apiservice.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const apiService = axios.create({
-    baseURL: "http://localhost:8080",
-    timeout: 20000,
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 20000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiService;
